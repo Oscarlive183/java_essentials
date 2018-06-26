3d6f12330a5bf6d5696468648cd3bbe88f0b9d4a
Oscarlive183 <oscarlive183@hotmail.com>
2018-06-25 20:38:59 -0600

    Adds EjemploAsignacion.java

---
+++ src/helloWorld/EjemploAsignacion.java
@@ -0,0 +1,25 @@
+package helloWorld;
+
+public class EjemploAsignacion {
+
+	public static void main(String[] args) {
+		int edad;
+		edad = 28;
+		double salario = 60000;
+		boolean isAdult;
+		if(edad >= 18) {
+			isAdult = true;
+		} else {
+			isAdult = false;
+		}
+				
+		String nombre;
+		nombre = "Pedro";
+		String apellidoPaterno = "Villarreal";
+		String apellidoMaterno = "Gonzalez";
+		System.out.println("Mi nombre completo es: " + nombre + " " + apellidoPaterno + " " + apellidoMaterno);
+		// TODO Auto-generated method stub
+
+	}
+
+}
